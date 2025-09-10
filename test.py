58e61630c30df6848ddfec333b90a4aac850e298
joseph <202172013@uich.edu.mx>
2025-09-10 11:35:12 -0600

    segunda prueba de streamlit

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import streamlit as st
-print("esto es una prueba")
+##print("esto es una prueba")
 
 st.set_page_config(page_title="UIEPCH", page_icon="📄")
-st.header('Hola Estudiante')
+st.title('Hola Estudiante')
 st.write('Soy el asistente oficial de la UICH, ¿en que puedo servirte?')
-
-
+st.text_area("Escribe una pregunta :)")
+st.button('Enviar')
 
 ##crear un entorno virtual evnv
 ##instalar dependencias
