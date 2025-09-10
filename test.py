302c521b601045de6e913ebbdf1907c7be69fccc
joseph <202172013@uich.edu.mx>
2025-09-10 13:59:54 -0600

    ocultocarpetavenv

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-import streamlit as st
+##import streamlit as st
 ##print("esto es una prueba")
 
-st.set_page_config(page_title="UIEPCH", page_icon="📄")
-st.title('Hola Estudiante')
-st.write('Soy el asistente oficial de la UICH, ¿en que puedo servirte?')
-st.text_area("Escribe una pregunta :)")
-st.button('Enviar')
+##st.set_page_config(page_title="UIEPCH", page_icon="📄")
+##st.title('Hola Estudiante')
+#3st.write('Soy el asistente oficial de la UICH, ¿en que puedo servirte?')
+##st.text_area("Escribe una pregunta :)")
+##st.button('Enviar')
+##st.image("mascota sin fondo reflejo.png")
 
 ##crear un entorno virtual evnv
 ##instalar dependencias
