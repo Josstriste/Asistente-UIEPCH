fef2b90e4f4cb0416cedf6741e3b461556fbdbda
joseph <202172013@uich.edu.mx>
2026-02-25 19:35:26 -0600

    modified pages

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-st.title("Page 1")
+st.set_page_config(page_title="UIEPCh", page_icon="🤖", layout="centered")
+st.logo("images/v2Logo.png", size='large')
+
+page_chat = st.Page("chat.py", title="Asistente Virtual", icon="💬", default=True)
+page_info = st.Page("info.py", title="Acerca de UICh", icon="🏫")
+
+pg = st.navigation(
+    [page_chat,page_info]
+
+)
+
+pg.run()
---
+++ info.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.markdown(
+    "<h1 style='text-align: center; font-size: 4em; margin-bottom: 0; padding-bottom: 0;'>Acerca de</h1>", unsafe_allow_html=True
+    )
+
+st.markdown(
+    "<p <h1 style='text-align: justify; font-size: 1.5em; margin-top: 2rem; padding-bottom: 0;'>Somos una Institución de Educación Superior que forma profesionales de calidad, con actitud científica, creativa, solidaria, con espíritu emprendedor e innovador, sensibles a la diversidad cultural y comprometidos con el respeto a la valoración de la diferentes culturas formando profesionistas acorde a las demandas de la sociedad y desarrollando actividades académicas, de investigación, difusión y extensión universitaria, para beneficiar a la Región, el Estado y el País, con la razón de mejorar el desarrollo humano en los nuevos retos de nuestra era.</p>",
+    unsafe_allow_html=True
+    )
