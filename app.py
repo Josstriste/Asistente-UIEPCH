713c62f43cb9ebeeccaf22cddaf8b22440730930
joseph <202172013@uich.edu.mx>
2026-02-26 18:44:21 -0600

    new design for pages

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import streamlit as st
 
 st.set_page_config(page_title="UIEPCh", page_icon="🤖", layout="centered")
-st.logo("images/v2Logo.png", size='large')
+
+st.logo("images/logoUich.png")
+
+st.html("""
+  <style>
+    img[data-testid="stSidebarLogo"] {
+        width: 7.75rem;
+        height: 3.2rem;
+        margin-top: 1.5em;
+        max-height: none !important;
+        max-width: none !important;
+    }    
+  </style>
+""")
 
 page_chat = st.Page("chat.py", title="Asistente Virtual", icon="💬", default=True)
 page_info = st.Page("info.py", title="Acerca de UICh", icon="🏫")
 
 pg = st.navigation(
     [page_chat,page_info]
-
 )
 
 pg.run()
